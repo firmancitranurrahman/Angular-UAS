2c478b9d879a63854ebdebbc6c7a383e9f712640
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-05-19 17:06:42 +0700

    import http adn from

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactmeComponent } from './contactme/contactme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ContactmeComponent
   ],
   imports: [
     BrowserModule,
